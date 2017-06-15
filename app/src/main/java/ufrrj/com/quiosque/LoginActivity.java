db507aaf7c2bfea39c9b1b565ed2ef12d7005208
Robert <robert.gol.jr@gmail.com>
2017-06-15 19:20:13 -0300

    Renomeando activity_login

--- app/src/main/java/ufrrj/com/quiosque/LoginActivity.java
+++ app/src/main/java/ufrrj/com/quiosque/LoginActivity.java
@@ -18,7 +18,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         matricula = (EditText) findViewById(R.id.login_matricula);
         password = (EditText) findViewById(R.id.login_password);
