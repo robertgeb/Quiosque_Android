51316456aa54b738446c292152c123332e1f770c
Robert <robert.gol.jr@gmail.com>
2017-06-15 15:39:17 -0300

    Removendo codigo criado automaticamente de exemplo pra IntentService

--- app/src/main/java/ufrrj/com/quiosque/QuiosqueService.kt
+++ app/src/main/java/ufrrj/com/quiosque/QuiosqueService.kt
@@ -13,15 +13,11 @@ import java.lang.UnsupportedOperationException
 
 
 /**
- * An [IntentService] subclass for handling asynchronous task requests in
- * a service on a separate handler thread.
- *
- *
- * TODO: Customize class - update intent actions, extra parameters and static
- * helper methods.
+ * Realiza as requisições referentes ao Quiosque paralelamente
  */
 class QuiosqueService : IntentService("QuiosqueService") {
 
+    // URL base do quiosque
     val QUIOSQUE_URL = "http://academico.ufrrj.br/quiosque/aluno/quiosque.php"
 
     override fun onHandleIntent(intent: Intent?) {
@@ -31,95 +27,53 @@ class QuiosqueService : IntentService("QuiosqueService") {
                 val matricula = intent.getStringExtra(EXTRA_MATRICULA)
                 val senha = intent.getStringExtra(EXTRA_SENHA)
                 handleActionLogin(matricula, senha)
-            } else if (ACTION_BAZ == action) {
-                val param1 = intent.getStringExtra(EXTRA_MATRICULA)
-                val param2 = intent.getStringExtra(EXTRA_SENHA)
-                handleActionBaz(param1, param2)
             }
         }
     }
 
     /**
-     * Handle action Foo in the provided background thread with the provided
-     * parameters.
+     * Realiza o Login com a matricula e a senha
      */
     private fun handleActionLogin(matricula: String, senha: String) {
-
+        // Fazendo o login e recebendo a resposta
         val doc: Document = Jsoup.connect(QUIOSQUE_URL)
                 .data("edtIdUs", matricula)
                 .data("edtIdSen", senha)
                 .data("btnIdOk", "Ok")
                 .userAgent("Mozilla")
                 .post()
-        Log.w("QuiosqueService", doc.toString())
-
+        // Obtendo gerenciador de notificações
         val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        var mNotificationId = 1
-
+        // Variável para alterar a id de cada notificação
+        var noticiasNotificationId = 1
+        // ArrayList com todos os elementos de notícia
         val noticiasElem = doc.getElementsByClass("item_noticias_dest")
-
+        // Loop pelas notícias
         for (noticia in noticiasElem){
-            Log.w("QuiosqueService", "Noticia: " + noticia.text())
+            // Criando notificação
             val notification = NotificationCompat.Builder(this)
                     .setContentTitle("Nova notícia")
-                    .setSubText(noticia.text())
+                    .setContentText(noticia.text())
                     .setSmallIcon(R.color.colorPrimary)
                     .build()
-
-
-            notificationManager.notify(mNotificationId++, notification)
+            // Enviando notificação e alterando ID da próxima notificação
+            notificationManager.notify(noticiasNotificationId++, notification)
         }
 
-
-    }
-
-    /**
-     * Handle action Baz in the provided background thread with the provided
-     * parameters.
-     */
-    private fun handleActionBaz(param1: String, param2: String) {
-        // TODO: Handle action Baz
-        throw UnsupportedOperationException("Not yet implemented")
     }
 
     companion object {
-        // TODO: Rename actions, choose action names that describe tasks that this
-        // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-        private val ACTION_LOGIN = "ufrrj.com.quiosque.action.LOGIN"
-        private val ACTION_BAZ = "ufrrj.com.quiosque.action.BAZ"
 
-        // TODO: Rename parameters
+        private val ACTION_LOGIN = "ufrrj.com.quiosque.action.LOGIN"
+
         private val EXTRA_MATRICULA = "ufrrj.com.quiosque.extra.MATRICULA"
         private val EXTRA_SENHA = "ufrrj.com.quiosque.extra.SENHA"
 
-        /**
-         * Starts this service to perform action Foo with the given parameters. If
-         * the service is already performing a task this action will be queued.
-
-         * @see IntentService
-         */
-        // TODO: Customize helper method
         fun startActionLogin(context: Context, matricula: String, senha: String) {
             val intent = Intent(context, QuiosqueService::class.java)
             intent.action = ACTION_LOGIN
             intent.putExtra(EXTRA_MATRICULA, matricula)
             intent.putExtra(EXTRA_SENHA, senha)
-            context.startService(intent)
-        }
-
-        /**
-         * Starts this service to perform action Baz with the given parameters. If
-         * the service is already performing a task this action will be queued.
-
-         * @see IntentService
-         */
-        // TODO: Customize helper method
-        fun startActionBaz(context: Context, param1: String, param2: String) {
-            val intent = Intent(context, QuiosqueService::class.java)
-            intent.action = ACTION_BAZ
-            intent.putExtra(EXTRA_MATRICULA, param1)
-            intent.putExtra(EXTRA_SENHA, param2)
             context.startService(intent)
         }
     }
